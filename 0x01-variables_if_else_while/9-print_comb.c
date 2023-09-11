22afd75e7d49e98bda0515cf2f1f90f5bd0bec91
Olabiisi <olasunkanmiolabisi31@yahoo.com>
2023-09-11 15:00:12 -0700

    This program prints all single digit numbers seperated by comma and space

--- 0x01-variables_if_else_while/9-print_comb.c
+++ 0x01-variables_if_else_while/9-print_comb.c
@@ -1,28 +1,28 @@
 /**
- * main - Entry point of the program
- *
- * Description: This program prints all single-digit numbers
- * separated by a comma and a space, followed by a newline.
- *
- * Return: Always 0 (Success)
- */
+* main - Entry point of the program
+*
+* Description: This program prints all single-digit numbers
+* separated by a comma and a space, followed by a newline.
+*
+* Return: Always 0 (Success)
+*/
 #include <stdio.h>
 
 int main(void)
 {
-    int i;
+int i;
 
-    for (i = 0; i < 10; i++)
-    {
-        putchar(i + 48);
-        if (i != 9)
-        {
-            putchar(44);
-            putchar(32);
-        }
-    }
+for (i = 0; i < 10; i++)
+{
+putchar(i + 48);
+if (i != 9)
+{
+putchar(44);
+putchar(32);
+}
+}
 
-    putchar(10);
+putchar(10);
 
-    return (0);
+return (0);
 }
