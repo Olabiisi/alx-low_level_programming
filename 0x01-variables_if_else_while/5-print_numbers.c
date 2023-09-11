636a1ab8ad6c8d7b969bc359a81bc64218ec609e
Olabiisi <olasunkanmiolabisi31@yahoo.com>
2023-09-10 19:37:44 -0700

    This program print s single digit numbers of base 10

---
+++ 0x01-variables_if_else_while/5-print_numbers.c
@@ -0,0 +1,22 @@
+#include <stdio.h>
+
+/**
+* main - Entry point
+*
+* Description: This program prints single-digit numbers of base 10,
+*              followed by a newline character.
+*
+* Return: Always 0 (Success)
+*/
+int main(void)
+{
+int digit;
+
+for (digit = '0'; digit <= '9'; digit++)
+{
+putchar(digit + 0);
+}
+putchar('\n');
+
+return (0);
+}
