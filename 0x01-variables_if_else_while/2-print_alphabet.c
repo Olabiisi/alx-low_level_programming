2273300f59a20d3ba098283b061d0562091a7ce9
Olabiisi <olasunkanmiolabisi31@yahoo.com>
2023-09-10 18:42:39 -0700

    This project prints the alphabet in small letters, followed by a new line

--- 0x01-variables_if_else_while/2-print_alphabet.c
+++ 0x01-variables_if_else_while/2-print_alphabet.c
@@ -1,5 +1,13 @@
 #include <stdio.h>
 
+/**
+* main - Entry point
+*
+* Description: This program prints the lowercase alphabet from a to z
+*              followed by a newline character.
+*
+* Return: Always 0 (Success)
+*/
 int main(void)
 {
 char letter;
