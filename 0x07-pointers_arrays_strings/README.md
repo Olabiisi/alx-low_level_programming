f82e28574569fd722235fde82c2bd8e316bb0fa7
Olabiisi <olasunkanmiolabisi31@yahoo.com>
2023-09-25 14:05:24 -0700

    This function fills memory with a constant byte

---
+++ 0x07-pointers_arrays_strings/README.md
@@ -0,0 +1,1 @@
+This deirectory contains even more projects on strings, array and pointers
---
+++ 0x07-pointers_arrays_strings/main.h
@@ -0,0 +1,9 @@
+#ifndef MAIN_H
+#define MAIN_H
+
+/* Function prototypes */
+char *_memset(char *s, char b, unsigned int n);
+void simple_print_buffer(char *buffer, unsigned int size);
+int _putchar(char c);
+
+#endif /* MAIN_H */
