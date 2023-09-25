18f48f24bd134d224c4e0b456209cd5395b843a7
Olabiisi <olasunkanmiolabisi31@yahoo.com>
2023-09-25 16:33:08 -0700

    this function sets the value of a pointer to a char

--- 0x07-pointers_arrays_strings/main.h
+++ 0x07-pointers_arrays_strings/main.h
@@ -1,12 +1,14 @@
 #ifndef MAIN_H
 #define MAIN_H
 
-#include <stddef.h>
+#include <stdio.h>
 
-int _putchar(char c);
-
-/* Prototypes for your functions go here */
-
+void _putchar(char c);
+unsigned int _strspn(char *s, char *accept);
+char *_strpbrk(char *s, char *accept);
+char *_strstr(char *haystack, char *needle);
+void print_chessboard(char (*a)[8]);
 void print_diagsums(int *a, int size);
+void set_string(char **s, char *to);
 
 #endif /* MAIN_H */
