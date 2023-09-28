01a8ed5ff8579e08601e2d28cfcf9a0509612da6
Olabiisi <olasunkanmiolabisi31@yahoo.com>
2023-09-28 10:15:00 -0700

    This function compares two strings and returns 1 if the strings can be considered identical, otherwise return 0

--- 0x08-recursion/main.h
+++ 0x08-recursion/main.h
@@ -1,7 +1,7 @@
 #ifndef MAIN_H
 #define MAIN_H
 
-/* Function prototype for is_palindrome */
-int is_palindrome(char *s);
+/* Function prototype for wildcmp */
+int wildcmp(char *s1, char *s2);
 
 #endif /* MAIN_H */
